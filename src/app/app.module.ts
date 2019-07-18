fa985ebcb5daeecf51e199fcf27d126745e9bba2
Marlon Marques <marlon.belohd@gmail.com>
2019-07-18 01:55:18 -0300

    Mostrando dados e imagem do cliente para pagina de profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { StorageService } from '../services/storage.service';
     CategoriaService,
     AuthService,
     StorageService,
+    ClienteService,
     ErrorInterceptorProvider
   ]
 })
